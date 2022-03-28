5e1be5308ab84b4f674810d7eb3a6cc1398c142b
AdityaSri11 <srikant2@purdue.edu>
2022-03-28 14:12:54 -0400

    Test

--- website/auth.py
+++ website/auth.py
@@ -8,6 +8,21 @@ auth = Blueprint('auth' , __name__)
 
 @auth.route('/login', methods=['GET' , 'POST'])
 def login():
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!' , category = 'success')
+            else:
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('Account assosiated with email not found' , category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -23,7 +38,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Account with email already exists!' , category='error')
+        elif len(email) < 4:
             flash('Email must be valid', category='error')
         elif len(firstName) < 2:
             flash('Name must be longer than two characters' , category='error')
